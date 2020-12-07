fe5335db2ca12632cadbac4ed6a7f448b42d67b7
Devin Gibson <dgib21@hotmail.com>
2020-12-06 23:10:56 -0600

    added controller logic

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -10,17 +10,6 @@ const {
   removeFriend,
 } = require("../../controllers/user-controller");
 
-const router = require("express").Router();
-
-const {
-  getAllUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
-  addFriend,
-  removeFriend,
-} = require("../../controllers/user-controller");
 
 // Set up GET all and POST at /api/users
 router.route("/").get(getAllUsers).post(createUser);
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect('mongodb://localhost/SocialNetworkChallenge/mongooseSocialNetwork',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mongooseSocialNetwork',
 {
     useFindAndModify: false,
     useNewUrlParser: true,
